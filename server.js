bcb8151b006b790754b5364d3e6857cfca0a8dc3
Halsehlawi <halsehalwi@outlook.com>
2025-07-13 16:08:52 +0300

    style sheet

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path')
 
 const app = express();
 const PORT = 3000;
 
+//MIDDLEWARE
+app.use(express.static(path.join(__dirname,'public')))
 app.use(morgan('dev'));
 
 //ROUTES
 app.get('/',(req,res)=>{
-    res.send('Welcome');
-})
+    res.render('index.ejs');
+});
 
 app.listen(PORT,()=>{
     console.log('Server running on port ', PORT)
-})
-
+});
